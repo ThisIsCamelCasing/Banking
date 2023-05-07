7f4bd80996f1f9171ac3b7d61a5cd0a34f7c2493
Saif Mohammed <kebabeater12@gmail.com>
2023-05-06 22:37:41 -0500

    yo

--- banking_code.py
+++ banking_code.py
@@ -2,4 +2,10 @@ import mysql.connector
 
 connection = mysql.connector.connect(user = "root", database = "elite102", password="saif2007-")
 
-#commit test
+from tkinter import *
+
+root = Tk()
+
+label = Label(root, text="")
+print("Hello! This is the Wells Fargo Bank App")
+
